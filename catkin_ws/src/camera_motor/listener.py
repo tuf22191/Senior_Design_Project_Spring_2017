547fe3cc5d73f33224d80965b0cf9a2b0559461a
Ali <tuf22191@temple.edu>
2017-03-15 18:06:28 -0400

    Got the actual message data transferred between nodes

--- catkin_ws/src/camera_motor/listener.py
+++ catkin_ws/src/camera_motor/listener.py
@@ -7,33 +7,26 @@ from numpy.linalg import inv
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Time
-import time
+#from Prediction import Prediction
 
-counter = 200
-counter2 = 0
-sum = 0.0
-deltaT = 0.0
+
 
 def callback(data):
-        # 	rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-        #rospy.loginfo("I heard %s , %s", data.data, data.data.nsecs)
-        py_time = rospy.Time.from_sec(time.time())
-        deltaT = (py_time-data.data).to_nsec()/1000000000.0
-# 	rospy.loginfo("Talker:%s.%s  Listener:%s.%s", data.data.secs, data.data.nsecs, py_time.secs, py_time.nsecs) 
- 	rospy.loginfo("%s+", deltaT)              	
-# 	rospy.loginfo("Talker:%s  Listener:%s  Diff(secs):%s ", data.data,  py_time, deltaT)              	
- #       counter2 = counter-1
-#	counter = counter2
-        #sum = sum +deltaT
-	#if(counter ==0):
-	#	rospy.loginfo("\n\n Avg. = %s", sum/200.0)
-	#	counter = 200
-	#	sum = 0.0 
+
+    x =str(data.data).split(',')
+    #y = [float(i) for i in x]
+    print x
+    print '%f' % float(x[0])
+    print '%f' % float(x[1])
+    print '%f' % float(x[2])
+    print '%f' % float(x[3])
+
 
 def listener():
-	rospy.init_node('listener',anonymous=True)
-	rospy.Subscriber("chatter",Time, callback)
-	rospy.spin()
+    rospy.init_node('listener',anonymous=True)
+    rospy.Subscriber('camera_to_motor', String , callback)
+    rospy.spin()
+
 
 if __name__ == '__main__':
-	listener()
+    listener()
--- catkin_ws/src/camera_motor/talker.py
+++ catkin_ws/src/camera_motor/talker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 
-
+#import roslib
 #coppied and modified from tutorial:
 import numpy as np
 import pylab
@@ -9,25 +9,36 @@ import math
 import random
 from numpy.linalg import inv
 import rospy
+#import std_msgs.msg
+#from camera_motor.msg import Prediction
+from Prediction import Prediction
 from std_msgs.msg import String
-from std_msgs.msg import Time 
+from std_msgs.msg import Time
+#from std_msgs.msg import Prediction
 import time
 
 def talker():
-	#pub =rospy.Publisher('chatter',String ,queue_size=10)
-	pub =rospy.Publisher('chatter',Time ,queue_size=10)
-	rospy.init_node('talker', anonymous=True)
-	rate = rospy.Rate(10) #10 hz
-	while not rospy.is_shutdown():
-		hello_str =  rospy.Time.from_sec(time.time()) # http://docs.ros.org/diamondback/api/rospy/html/rospy.rostime.Time-class.html
-		#hello_str = "Hello world %s" % rospy.get_time()
-		rospy.loginfo(hello_str)
-		pub.publish(hello_str)
-		rate.sleep()
+    x_vel = 100232.4123
+    y_vel = 1234.16759
+    msg_sent_time = "%f" % rospy.Time.from_sec(time.time()).to_sec()  #infloat
+    time_to_impact = 343.4232
+
+    our_msg=str(x_vel)+ ','+str(y_vel)+','+str(msg_sent_time)+','+str(time_to_impact)
+    pub =rospy.Publisher('camera_to_motor', String ,queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+
+    #rate = rospy.Rate(10) #10 hz
+    while not rospy.is_shutdown():
+
+        rospy.loginfo("Here: "+our_msg)
+        pub.publish(our_msg)
+        #pub.publish(our_msg)
+        rospy.sleep(1.0);
+        #rate.sleep()
 
 if __name__=='__main__':
-	try:
-		talker()
-	except rospy.ROSInterruptException:
-		rospy.loginfo("had rospy exception")
-		pass
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("had rospy exception")
+        pass
